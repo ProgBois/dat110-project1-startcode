79520ef251cefb796d8ab25edd7b910f878b6e94
HVL582123 <54101091+HVL582123@users.noreply.github.com>
2021-01-27 10:56:46 +0100

    Test

--- dat110-project1/src/no/hvl/dat110/TODO.java
+++ dat110-project1/src/no/hvl/dat110/TODO.java
@@ -4,6 +4,7 @@ public class TODO {
 
 	public static String method() {
 		//kommentar12
+		//GainzCity
 		String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
 		
 		return "Metoden " + methodName + " er ikke implementert";
